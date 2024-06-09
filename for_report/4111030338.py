e310b29c8d7bf28b840864ec64cad5e98d5fe317
JRay <lin299579@gmail.com>
2024-06-09 13:45:15 +0800

    ilearning file

---
+++ for_report/4111030338.py
@@ -0,0 +1,14 @@
+# 由於檔案太多，檔案沒辦法只以一個main.py 上傳
+# 必須從github clone repository
+
+# 開發環境
+# python 3.12.3, Windows 11 23H2
+
+# 需要的指令
+# git clone https://github.com/JRay-Lin/community-health.git
+# pip install -r requirements.txt
+
+# python main.py
+# 或者可以使用 start.bat 直接執行檔案
+
+# !!! 範例檔案存在 "李四" 名下，其他使用者沒有範例的健康數據 !!!
